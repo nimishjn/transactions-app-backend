57baa586d2dcbada873fb9d4761aeb2a12b986a8
nimishjn <nimishjain100701@gmail.com>
2021-12-22 00:46:09 +0530

    feat: Basic structure

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+require("dotenv/config");
+require("./models/dbInit");
+
+const cors = require("cors");
+app.use(cors());
+
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// const authMiddleware = require('./middleware/authorize');
+
+// const loginRoute = require("./routes/login");
+// app.use("/login", loginRoute);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+      test2: "app.js issue",
+    },
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Server started!");
+});
---
+++ tools/responseCodes.js
@@ -0,0 +1,14 @@
+const error_codes = {
+  E0: "Undefined error occured",
+};
+
+const logical_errors = {
+  L0: "Incorrect password",
+};
+
+const success_codes = {
+  S0: "Signup successful",
+  S1: "Login successful",
+};
+
+export default { error_codes, logical_errors, success_codes };
